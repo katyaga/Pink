8185bb0920d5ae0afab9000bfd94680809793ed5
user <aevakatya@gmail.com>
2019-06-14 13:59:52 +0700

    Pixel perfect 3-х страниц

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -17,3 +17,44 @@ navToggle.addEventListener('click', function() {
     header.classList.add('page-header--nav-closed');
   }
 });
+
+// Интерактивная карта
+// function getIconSize () {
+//   var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//
+//   if (width < 660) {
+//     return [36, 35];
+//   }
+//   else if (width < 768) {
+//     return [55, 53];
+//   } else {
+//     return [113, 106];
+//   }
+// }
+
+function getPlacemark(myMap) {
+  return new ymaps.Placemark([59.938631, 30.323055], {
+    hintContent: 'Собственный значок метки',
+    balloonContent: 'Магазин "CAT ENERGY"'
+  }, {
+    iconImageHref: "img/map-marker-svg.svg",
+    iconLayout: 'default#image',
+    iconImageSize: [36, 35],
+    iconOffset: getIconSize().map(function (item) {return -0.5 * item}),
+  });
+}
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [59.938631, 30.323055],
+    zoom: 15
+  });
+
+  myMap.geoObjects.add(getPlacemark(myMap));
+
+  window.onresize = function(event) {
+    myMap.geoObjects.removeAll();
+    myMap.geoObjects.add(getPlacemark(myMap));
+  };
+}
